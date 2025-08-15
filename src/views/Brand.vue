3a85e1c8559cd948cb81658a58e7d1e709667da9
tang408 <karta3255834@gmail.com>
2025-08-15 23:49:39 +0800

    revise

--- src/views/Brand.vue
+++ src/views/Brand.vue
@@ -1,10 +1,11 @@
 <template>
-  <Banner text="品牌刊登" image="/src/assets/images/banner-example.jpg" />
+  <Banner text="品牌刊登" :image="bannerImg" />
   <Main></Main>
 </template>
 
 <script setup>
 import Banner from "@/components/Shared-Banner.vue";
 import Main from "@/components/brand/Main.vue";
+import bannerImg from "@/assets/images/banner-example.jpg";
 </script>
 <style lang="scss" scoped></style>
--- src/views/Cooperative.vue
+++ src/views/Cooperative.vue
@@ -1,10 +1,11 @@
 <template>
-  <Banner text="合作品牌" image="/src/assets/images/banner-example.jpg" />
+  <Banner text="合作品牌" :image="bannerImg" />
   <Main></Main>
 </template>
 
 <script setup>
 import Banner from "@/components/Shared-Banner.vue";
 import Main from "@/components/cooperative/Main.vue";
+import bannerImg from "@/assets/images/banner-example.jpg";
 </script>
 <style lang="scss" scoped></style>
